a0af8b6b8600543f130945bf50980c7bc4844035
HEYlime <1844467642@qq.com>
2022-05-19 17:34:27 +0800

    svg修改

--- src/views/example/Svg.vue
+++ src/views/example/Svg.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     svg
-    <LSvg iconName="home" color="#ff7f50" />
+    <LSvg iconName="home" color="#ff7f50" className="external_class" />
     <LSvg iconName="home" color="#00008b" />
   </div>
 </template>
@@ -14,3 +14,9 @@ export default {
   },
 };
 </script>
+<style lang="less" scoped>
+.external_class {
+  width: 48px;
+  height: 48px;
+}
+</style>
